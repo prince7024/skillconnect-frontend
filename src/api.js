467a832e3a928023fbe2739d6082b2addfbbcf60
Prince Kumar Choudhary <princekumarchoudhary22@gmail.com>
2025-11-03 22:25:29 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://skillconnect-backend-3e6v.onrender.com", 
+  baseURL: "https://skillconnect-backend-3e6v.onrender.com/api", 
 });
 
 API.interceptors.request.use((config) => {
